943fb954717b2659d9886a6f29a64cbca2bd9eee
rubbos <41753645+rubbos@users.noreply.github.com>
2024-12-21 19:15:53 +0100

    first hm commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Rub's home manager configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations = {
+        myprofile = "rub" {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, ... }:
+{
+  home = {
+    packages = with pkgs; [
+      hello
+    ];
+
+    username = "rub";
+    homeDirectory = "/home/rub";
+
+    # Don't ever change this after the first build.  Don't ask questions.
+    stateVersion = "24.11";
+  };
+}
