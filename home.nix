2d30bfee8ec5b80e4c88b4d4bd19a9aa26583b5c
rubbos <41753645+rubbos@users.noreply.github.com>
2024-12-25 20:44:08 +0100

    added tmux

--- home.nix
+++ home.nix
@@ -8,10 +8,12 @@
         vim
         kitty
         wezterm
+	tmux
         neovim
         git
         lazygit
         neofetch
+	pfetch-rs
         wget
         wofi
         waybar
@@ -27,6 +29,51 @@
     ];
     # Don't change this after the first build
     stateVersion = "24.11";
+  };
+
+  programs.tmux = {
+    enable = true; 
+    extraConfig = ''
+	unbind r
+	bind r source-file ~/.tmux.conf
+
+	set -g @plugin 'tmux-plugins/tpm'
+	set -g @plugin 'tmux-plugins/tmux-sensible'
+	set -g @plugin 'tmux-plugins/tmux-resurrect'
+	set -g @plugin 'tmux-plugins/tmux-continuum'
+	set -g @plugin 'dracula/tmux'
+
+	set -g @resurrect-strategy-nvim 'session'
+	set -g @continuum-restore 'on'
+
+	unbind C-b
+	set-option -g prefix C-Space
+	bind-key C-Space send-prefix
+
+	bind | split-window -h
+	bind - split-window -v
+	unbind '"'
+	unbind %
+
+	set -g mouse on
+	set -g default-terminal "tmux-256color"
+
+	set-option -sa terminal-overrides ",xterm*:Tc"
+	set-option -g status-position top
+
+	set -g @dracula-plugins "cpu-usage gpu-usage ram-usage network-ping"
+	set -g @dracula-cpu-usage-colors "light_purple dark_gray"
+	set -g @dracula-network-ping-colors "yellow dark_gray"
+	set -g @dracula-show-left-icon " "
+	set -g @dracula-show-timezone false
+	set -g @dracula-day-month true
+	set -g @dracula-time-format "%F %R"
+	set -g @dracula-show-location false
+	set -g @dracula-show-fahrenheit false
+
+	run -b '~/.tmux/plugins/tpm/tpm'
+      "
+    '';
   };
 
   # Move the dotfiles config inside home.file
